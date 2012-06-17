238f74354e2a7d7141aebaa3ae082d1591c44837
Brian Taylor <el.wubo@gmail.com>
2012-06-16 22:03:25 -0400

    capture #src deps as well

--- build_tools/lzz_deps.pl
+++ build_tools/lzz_deps.pl
@@ -19,32 +19,48 @@ foreach (@lzzs) {
 }
 
 # find any cases where they're included
-my @includes = ();
+my @h_includes = ();
+my @c_includes = ();
+
 open(my $fh, $filename);
 
-my $disabled = 0;
+my $src_includes = 0;
 foreach my $line (<$fh>) {
-    if(!$disabled && $line =~ s/#include "([^"]+)"$/$1/) {
+    if($line =~ s/#include "([^"]+)"$/$1/) {
         chomp($line);
         foreach my $lzz (@lzzs) {
             if ($line eq $lzz) {
-                push @includes, $line;
+                if($src_includes) {
+                    push @c_includes, $line;
+                } else {
+                    push @h_includes, $line;
+                }
             }
         }
     } elsif($line =~ /#src$/) {
-        $disabled = 1;
+        $src_includes = 1;
     } elsif($line =~ /#end$/) {
-        $disabled = 0;
+        $src_includes = 0;
     }
 }
 
 close($fh);
 
 open(my $of, ">$outfile");
-$filename =~ s/\.lzz$/\.h/g;
 
-if(@includes) {
-    print $of "$filename $outfile : " . join(' ', @includes) . "\n";
+my $h_filename = $filename;
+$h_filename =~ s/\.lzz$/\.h/;
+
+my $c_filename = $filename;
+$c_filename =~ s/\.lzz$/\.cpp/;
+
+if(@h_includes) {
+    print $of "$h_filename $outfile : " . join(' ', @h_includes) . "\n";
 }
+
+if(@c_includes) {
+    print $of "$c_filename $outfile : " . join(' ', @c_includes) . "\n";
+}
+
 close($of);
 
