9303eda02ccb878bddb309fc19e98ace70b2e755
Brian Taylor <el.wubo@gmail.com>
2012-06-17 15:22:39 -0400

    initial coding

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "common.h"
 
-#include <QtGui>
 #include <iostream>
+#include <cassert>
 
-#include "Canvas.h"
+#include "Graph.h"
+#include "Node.h"
+#include "Edge.h"
 
 using namespace std;
 
@@ -11,9 +13,16 @@ int main(int argc, char ** argv) {
   GC_INIT();
   GC_enable_incremental();
 
-  QApplication app(argc, argv);
-  Canvas canvas;
-  canvas.show();
+  Graph graph;
+  Node * one = graph.addNode(new Node("one"));
+  Node * two = graph.addNode(new Node("two"));
 
-  return app.exec();
+  assert(one == graph.findNode("one"));
+  assert(two == graph.findNode("two"));
+
+  graph.addEdge(new Edge(one, two));
+
+  BOOST_AUTO(nodes, graph.nodesFrom(one));
+  assert(nodes.size() == 1);
+  assert(two == *(nodes.begin()));
 }
